47aa52acbe9850d792c27fe3b3d37527866780a3
saddaul-siam <saddadul.siam@gmail.com>
2021-10-29 14:32:10 +0600

    added delete method

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 var cors = require('cors');
 const { MongoClient } = require('mongodb');
 require('dotenv').config();
-const objectId = require('mongodb').ObjectId;
+const ObjectId = require('mongodb').ObjectId;
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -15,25 +15,51 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 client.connect(err => {
-  const servicesCollection = client.db("roam-tourism").collection("services");
 
-  // get all services
+  const toursCollection = client.db("roam-tourism").collection("services");
+  const ordersCollection = client.db("roam-tourism").collection("orders");
+
+  // get all tours
   app.get("/tours", async (req, res) => {
-    const result = await servicesCollection.find({}).toArray();
+    const result = await toursCollection.find({}).toArray();
     res.send(result);
   });
 
   // get single tour
   app.get("/singleTour/:id", async (req, res) => {
-    const result = await servicesCollection.findOne({ _id: objectId(req.params.id) });
+    const result = await toursCollection.findOne({ _id: ObjectId(req.params.id) });
     res.send(result);
   });
 
-  // post api
+  // add tours
   app.post("/addTours", async (req, res) => {
     console.log(req.body);
-    const result = await servicesCollection.insertOne(req.body)
+    const result = await toursCollection.insertOne(req.body)
     res.send(result.insertedId);
+  });
+
+  //add order in database
+  app.post("/addOrders", async (req, res) => {
+    const result = await ordersCollection.insertOne(req.body)
+    res.send(result);
+    console.log(result);
+  });
+
+  // get all order by email query
+  app.get("/myOrders/:email", async (req, res) => {
+    // console.log(req.params);
+    const result = await ordersCollection.find({ email: req.params.email })
+      .toArray()
+    res.send(result);
+  });
+
+  // delete products
+  app.delete("/deleteTours/:id", async (req, res) => {
+    console.log(req.params);
+
+    const result = await ordersCollection.deleteOne({ _id: req.params.id })
+    console.log(result);
+    res.send(result);
   });
 
 });
